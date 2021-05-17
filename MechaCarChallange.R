700836bbf3386bc6297bce0b725ab44a9897b971
Nuphar Ezra <nuphare@hotmail.com>
2021-05-16 22:20:48 -0700

    Message

--- MechaCarChallange.R
+++ MechaCarChallange.R
@@ -1,3 +1,5 @@
+## Deliverable 1
+
 #import dplyr library 
 library(dplyr)
 
@@ -9,3 +11,28 @@ lingress_model <- lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + gro
 
 #determine the p-value and the r-squared value for the linear regression model
 summary(lingress_model)
+
+## Deliverable 2
+
+#read in the Suspension_Coil csv
+Suspension_Coil <- read.csv(file='Suspension_Coil.csv',check.names =F, stringsAsFactors = F )
+
+#Write an RScript that creates a total_summary dataframe using the summarize() function 
+#to get the mean, median, variance, and standard deviation of the suspension coil’s PSI column.
+total_summary <- Suspension_Coil %>% summarize(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI),SD = sd(PSI))
+
+#Write an RScript that creates a lot_summary dataframe using the group_by() and the 
+#summarize() functions to group each manufacturing lot by the mean, median, variance, 
+#and standard deviation of the suspension coil’s PSI column
+lot_summary <- Suspension_Coil  %>% group_by(Manufacturing_Lot) %>% 
+  summarize(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI),SD = sd(PSI))
+
+
+
+
+
+
+
+
+
+
